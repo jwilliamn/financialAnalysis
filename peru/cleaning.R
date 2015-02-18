cabedce59cd21a6b0330612b6f4263ebe26d1860
williamn <hwilliamnq@gmail.com>
2015-02-18 14:16:46 -0500

    peruvian data

--- peru/cleaning.R
+++ peru/cleaning.R
@@ -3,5 +3,5 @@
 setwd('/home/williamn/Repository/financialAnalysis/peru')
 list.files()
 etna<- read.csv('etna.csv')
-etna<- as.data.frame(t(etna))
-write.csv(etna, file = "etnat.csv")
+#etna<- as.data.frame(t(etna))
+#write.csv(etna, file = "etnat.csv")
