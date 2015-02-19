cb702c7267316980775657ddf2e985dce9d49482
williamn <hwilliamnq@gmail.com>
2015-02-18 20:35:10 -0500

    peruvian company

--- peru/cleaning.R
+++ peru/cleaning.R
@@ -1,7 +1,12 @@
 # Data cleaning
 
 setwd('/home/williamn/Repository/financialAnalysis/peru')
-list.files()
-etna<- read.csv('etna.csv')
+listFiles<- list.files("data", full.names = TRUE)
+#etna<- read.csv('etna.csv')
 #etna<- as.data.frame(t(etna))
 #write.csv(etna, file = "etnat.csv")
+for(i in 1:70){
+    tmp<- read.csv(listFiles[i])
+    tmpt<- as.data.frame(t(tmp))
+    write.csv(tmpt, file = listFiles[i])
+}
