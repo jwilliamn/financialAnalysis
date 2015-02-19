28a5f64c9d7668ad7a6744809a151b60ee6e6397
williamn <hwilliamnq@gmail.com>
2015-02-19 13:37:47 -0500

    data transposed

--- peru/cleaning.R
+++ peru/cleaning.R
@@ -5,7 +5,7 @@ listFiles <- list.files("data", full.names = TRUE)
 #etna <- read.csv('etna.csv')
 #etna <- as.data.frame(t(etna))
 #write.csv(etna, file = "etnat.csv")
-for(i in 1:70){
+for(i in 1:68){
     name<- listFiles[i]
     tmp <- read.csv(listFiles[i])
     tmpt <- as.data.frame(t(tmp))
