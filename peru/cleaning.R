cfd6a98e07cef7288a2a799d159edd640af7dbba
williamn <hwilliamnq@gmail.com>
2015-02-19 13:07:15 -0500

    r code

--- peru/cleaning.R
+++ peru/cleaning.R
@@ -1,12 +1,13 @@
 # Data cleaning
 
 setwd('/home/williamn/Repository/financialAnalysis/peru')
-listFiles<- list.files("data", full.names = TRUE)
-#etna<- read.csv('etna.csv')
-#etna<- as.data.frame(t(etna))
+listFiles <- list.files("data", full.names = TRUE)
+#etna <- read.csv('etna.csv')
+#etna <- as.data.frame(t(etna))
 #write.csv(etna, file = "etnat.csv")
 for(i in 1:70){
-    tmp<- read.csv(listFiles[i])
-    tmpt<- as.data.frame(t(tmp))
-    write.csv(tmpt, file = listFiles[i])
+    name<- listFiles[i]
+    tmp <- read.csv(listFiles[i])
+    tmpt <- as.data.frame(t(tmp))
+    write.csv(tmpt, file = name)
 }
