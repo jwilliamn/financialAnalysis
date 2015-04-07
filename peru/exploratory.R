e605b77d9e14ea17c2de50b7eadee0c836f3d03d
williamn <hwilliamnq@gmail.com>
2015-04-07 16:19:16 -0500

    all years data

--- peru/exploratory.R
+++ peru/exploratory.R
@@ -4,9 +4,11 @@ setwd('/home/williamn/Repository/financialAnalysis/peru')
 listFiles <- list.files("cleanData", full.names = TRUE)
 
 examples <- data.frame()
-for(i in 1:length(listFiles)){
-    tmp <- read.csv(listFiles[i])
-    examples <- rbind(examples, tmp[which(tmp$year==2013),])
+for(y in 2013:2000){
+    for(i in 1:length(listFiles)){
+        tmp <- read.csv(listFiles[i])
+        examples <- rbind(examples, tmp[which(tmp$year==y),])
+    }
 }
 
 write.table(examples, file = "data.csv", sep = ",", quote = F, row.names = F)
@@ -20,7 +22,7 @@ library(ggthemes)
 ggplot(aes(x = ROA), data = datacl) + 
     geom_point(aes(y = REQ, color = Field)) + 
     theme_bw() + 
-    coord_cartesian(xlim = c(-0.25, 0.28), ylim = c(-0.5, 1)) + 
+    coord_cartesian(xlim = c(-0.5, 0.8), ylim = c(-2.5, 2.5)) + 
     ggtitle("Exploratory analysis")
 
 
